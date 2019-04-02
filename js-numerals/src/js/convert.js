a493fe19cb9c69e33bae16b3d22248dba1b50623
Carl Phillips <carl.phillips@gmail.com>
2019-04-02 20:41:58 +0200

    ConvertNumberToString TDD unit tests passed

--- js-numerals/src/js/convert.js
+++ js-numerals/src/js/convert.js
@@ -1,3 +1,54 @@
-export const hello = () => {
-    return "hello";
+export const convertNumberToString = num => {
+    const arr1to19 = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    ];
+
+    const arr10s = [
+        "zero",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    const arrDigits = [arr10s, arr1to19];
+
+    if (typeof num === "number") {
+        let str = num.toString();
+        if (str.length == 1 || num <= 19) {
+            return arr1to19[num];
+        } else if (str.length == 2) {
+            let arr = str.split("");
+            let strNumber = arr.map((digit, index) => {
+                return arrDigits[index][digit];
+            });
+            return strNumber.join("-");
+        }
+    }
+
+    return null;
 };
--- js-numerals/src/js/convert.test.js
+++ js-numerals/src/js/convert.test.js
@@ -1,7 +1,23 @@
-import { hello } from "./convert";
+import { convertNumberToString } from "./convert";
 
-describe("hello", () => {
-    it("should output hello", () => {
-        expect(hello()).toBe("hello");
+describe("convertNumberToString", () => {
+    it("should convert a number into a string", () => {
+        expect(convertNumberToString(0)).toBe("zero");
+        expect(convertNumberToString(1)).toBe("one");
+        expect(convertNumberToString(7)).toBe("seven");
+        expect(convertNumberToString(9)).toBe("nine");
+    });
+
+    it("should not convert a string into a number", () => {
+        expect(convertNumberToString("0")).toBe(null);
+        expect(convertNumberToString("one")).toBe(null);
+    });
+
+    it("should convert a 2-digit number into a string", () => {
+        expect(convertNumberToString(42)).toBe("forty-two");
+        expect(convertNumberToString(11)).toBe("eleven");
+        expect(convertNumberToString(19)).toBe("nineteen");
+        expect(convertNumberToString(55)).toBe("fifty-five");
+        expect(convertNumberToString(99)).toBe("ninety-nine");
     });
 });
